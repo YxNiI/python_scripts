c4ec1962bf4e8c30017da49b5190c4b22b668a75
Lyra Claus <lyra.claus@proton.me>
2024-03-10 20:52:59 +0100

    Made some additions to os_scanner.py and improved its structure a bit.

--- os_scanner.py
+++ os_scanner.py
@@ -1,17 +1,20 @@
-# Analyzes data of the operating-system.
 import os
+import platform
 
-outputs = []
 
-outputs.append("Operating-System: %s" % (os.name))
+# Output data about the operating-system.
 
-enviroVars = ["USERNAME", "HOME", "LOGNAME", "DESKTOP_SESSION" "SHELL"]
+outputs: list[str] = ["System-Information:\n", "Operating-System: %s" % platform.platform()]
+architecture_tuple: tuple[str, str] = platform.architecture()
+outputs.append("Architecture: %s %s" % (platform.machine(), architecture_tuple[0]))
+outputs.append("Linkage: %s" % architecture_tuple[1])
+outputs.append("Network-Name: %s" % platform.node())
 
-outputs.append("Enviroment-Variables:")
+outputs.append("\nEnvironment-Variables:")
+enviroVars: list[str] = ["USERNAME", "HOME", "LOGNAME", "DESKTOP_SESSION", "SHELL"]
 for key, value in os.environ.items():
-    if (key in enviroVars):
+    if key in enviroVars:
         outputs.append(key + ": " + value)
 
-clear = lambda: os.system('clear')
-clear()
+os.system('clear')
 print('\n'.join(outputs))
